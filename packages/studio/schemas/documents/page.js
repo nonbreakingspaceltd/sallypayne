c129abd447b482aa045aa992d4d93eab54f93e0e
Paul Welsh <spacedawwwg@gmail.com>
2021-08-13 23:33:27 +0100

    feat: add basic page schema

---
+++ packages/studio/schemas/documents/page.js
@@ -0,0 +1,22 @@
+export default {
+  name: 'page',
+  title: 'Page',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: ({ title }) => title,
+        maxLength: 200,
+      },
+      validation: (Rule) => Rule.required(),
+    },
+  ],
+};
