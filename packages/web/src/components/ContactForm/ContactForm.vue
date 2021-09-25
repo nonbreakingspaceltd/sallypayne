f2345cc413d1adb45d93163a57a7805136a2a402
Paul Welsh <spacedawwwg@gmail.com>
2021-09-25 17:42:07 +0100

    feat: remove js form submit

--- packages/web/src/components/ContactForm/ContactForm.vue
+++ packages/web/src/components/ContactForm/ContactForm.vue
@@ -1,7 +1,6 @@
 <template>
   <form
-    v-on:submit.prevent="handleSubmit"
-    action="/contact/success/"
+    action="/contact"
     class="c-contact-form"
     name="contact"
     method="POST"
@@ -17,43 +16,18 @@
     </p>
     <div class="c-contact-form__group">
       <label class="c-contact-form__label" for="formName">Full Name</label>
-      <input
-        class="c-contact-form__input"
-        type="text"
-        name="name"
-        id="formName"
-        v-model="formData.name"
-        required
-      />
+      <input class="c-contact-form__input" type="text" name="name" id="formName" required />
     </div>
     <div class="c-contact-form__group">
       <label class="c-contact-form__label" for="formEmail">Email Address</label>
-      <input
-        class="c-contact-form__input"
-        type="email"
-        name="email"
-        id="formEmail"
-        v-model="formData.email"
-        required
-      />
+      <input class="c-contact-form__input" type="email" name="email" id="formEmail" required />
     </div>
     <div class="c-contact-form__group">
       <label class="c-contact-form__label" for="formMessage">What would you like to say?</label>
-      <textarea
-        class="c-contact-form__textarea"
-        name="message"
-        id="formMessage"
-        required
-        v-model="formData.message"
-      ></textarea>
+      <textarea class="c-contact-form__textarea" name="message" id="formMessage" required />
     </div>
     <div class="c-contact-form__actions">
-      <Button
-        type="submit"
-        class="c-contact-form__submit"
-        :variants="['color-a']"
-        :disabled="submitting"
-      >
+      <Button type="submit" class="c-contact-form__submit" :variants="['color-a']">
         Send your message
       </Button>
     </div>
@@ -61,49 +35,11 @@
 </template>
 
 <script>
-import { ref } from 'vue';
 import Button from '../Button';
 
 export default {
   components: {
     Button,
-  },
-  setup() {
-    const formData = ref({
-      name: null,
-      email: null,
-      message: null,
-    });
-    const submitting = ref(false);
-
-    const encode = (data) => {
-      return Object.keys(data)
-        .map((key) => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`)
-        .join('&');
-    };
-
-    const handleSubmit = async (event) => {
-      try {
-        this.submitting = true;
-        await fetch('/', {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-          body: encode({
-            'form-name': event.target.getAttribute('name'),
-            ...formData,
-          }),
-        });
-        this.submitting = false;
-      } catch (error) {
-        throw new Error(error);
-      }
-    };
-
-    return {
-      formData,
-      submitting,
-      handleSubmit,
-    };
   },
 };
 </script>
