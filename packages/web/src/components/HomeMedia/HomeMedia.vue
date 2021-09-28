cbfa68fb9b2b665eddb58c00b01ce56375493464
Paul Welsh <spacedawwwg@gmail.com>
2021-09-28 17:18:32 +0100

    feat: add dragscroll

---
+++ packages/web/src/components/HomeMedia/HomeMedia.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="sp-c-home-media" v-dragscroll>
+    <img
+      src="/images/sally-payne-home.svg"
+      class="sp-c-home-media__image"
+      alt="Homepage Illustration of Charactacters"
+      width="1024"
+      height="525"
+    />
+  </div>
+</template>
+
+<script>
+import { dragscrollNext } from 'vue-dragscroll';
+
+export default {
+  name: 'HomeCanvas',
+  directives: {
+    dragscroll: dragscrollNext,
+  },
+};
+</script>
+
+<style lang="postcss">
+@import '../../assets/styles/settings';
+@import '../../assets/styles/tools';
+
+.sp-c-home-media {
+  width: 100%;
+  overflow: auto;
+  flex: 1;
+  padding: 0 var(--space-x-4);
+
+  @media (--mq-md) {
+    padding: 0 var(--space-x-6);
+  }
+
+  @media (--mq-lg) {
+    margin-top: auto;
+  }
+}
+
+.sp-c-home-media__image {
+  height: 100%;
+  width: auto;
+  max-width: none;
+  position: relative;
+  cursor: grab;
+
+  @media (--mq-md) {
+    margin: 0 auto;
+  }
+
+  @media (--mq-xxxl) {
+    max-width: 3000px;
+  }
+}
+</style>
