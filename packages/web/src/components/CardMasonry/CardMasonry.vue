0df9283deb5888b72f48d0fed1d46899a254361d
Paul Welsh <spacedawwwg@gmail.com>
2021-10-03 18:13:45 +0100

    feat: better masonry

--- packages/web/src/components/CardMasonry/CardMasonry.vue
+++ packages/web/src/components/CardMasonry/CardMasonry.vue
@@ -1,16 +1,21 @@
 <template>
-  <div :class="containerClassNames">
-    <div :class="classNames.item" v-for="(item, index) in items" :key="`masonryItem${index}`">
-      <div :class="classNames.content">
+  <div ref="container" :class="['sp-c-card-masonry', isJsMasonry && 'is-js-masonry']">
+    <div
+      v-for="(column, columnIndex) in columns"
+      :key="columnIndex"
+      class="sp-c-card-masonry__column"
+      :data-index="columnIndex"
+    >
+      <div v-for="itemIndex in column" :key="itemIndex" class="sp-c-card-masonry__item">
         <Card
-          :title="item.title"
-          :subtitle="item.subtitle"
-          :href="item.href"
-          :image="item.image"
-          :variants="[item.variant]"
+          :title="items[itemIndex].title"
+          :subtitle="items[itemIndex].subtitle"
+          :href="items[itemIndex].href"
+          :image="items[itemIndex].image"
+          :variants="[items[itemIndex].variant]"
         >
-          <p v-if="item.excerpt">
-            {{ item.excerpt }}
+          <p v-if="items[itemIndex].excerpt">
+            {{ items[itemIndex].excerpt }}
           </p>
         </Card>
       </div>
@@ -19,7 +24,7 @@
 </template>
 
 <script>
-import classNames from 'classnames';
+import { nextTick, onBeforeUnmount, onMounted, ref, toRefs } from 'vue';
 import Card from '../Card';
 
 export default {
@@ -28,78 +33,90 @@ export default {
   },
   props: {
     items: {
-      type: Array,
+      type: Object,
       required: true,
     },
   },
-  data() {
-    return {
-      masonryEvents: ['load', 'resize'],
-      classNames: {
-        container: 'sp-c-card-masonry',
-        item: 'sp-c-card-masonry__item',
-        content: 'sp-c-card-masonry__item__content',
-      },
-      isActive: false,
-      isCalculated: false,
-    };
-  },
-  watch: {
-    items() {
-      const vm = this;
-      vm.$nextTick(() => {
-        vm.resizeAllMasonryItems();
-      });
-    },
-  },
-  computed: {
-    containerClassNames() {
-      return classNames(this.classNames.container, {
-        'is-active': this.isActive,
-        'is-calculated': this.isCalculated,
-      });
-    },
-  },
-  methods: {
-    resizeMasonryItem(item) {
-      const computedStyle = window.getComputedStyle(this.$el);
-      const rowHeight = 1;
-      const rowGap = parseInt(computedStyle.getPropertyValue('grid-row-gap'));
-      const rowSpan = Math.ceil(
-        (item.querySelector(`.${this.classNames.content}`).getBoundingClientRect().height +
-          rowGap) /
-          (rowHeight + rowGap)
-      );
-      item.style.gridRowEnd = `span ${rowSpan}`;
-    },
-    resizeAllMasonryItems() {
-      var allItems = Array.from(this.$el.querySelectorAll(`.${this.classNames.item}`));
-      allItems.forEach((item) => {
-        this.resizeMasonryItem(item);
-      });
-      this.isCalculated = true;
-    },
-  },
-  mounted() {
-    const vm = this;
-    this.isActive = true;
-    if (!window.CSS.supports('grid-template-rows', 'masonry')) {
-      vm.masonryEvents.forEach((event) => {
-        window.addEventListener(event, vm.resizeAllMasonryItems);
-      });
-      this.resizeAllMasonryItems();
-      window.dispatchEvent(new Event('resize'));
+  setup(props) {
+    const resizeObserver = ref();
+    const { items } = toRefs(props);
+    const columns = ref([]);
+    const container = ref();
+    const columnWidth = ref(150);
+    const isJsMasonry = ref(false);
+
+    function columnCount() {
+      const containerStyles = getComputedStyle(container.value);
+
+      // create dummy element
+      const columnElement = document.createElement('div');
+      columnElement.classList.add('sp-c-card-masonry__column');
+      container.value.appendChild(columnElement);
+      const columnStyles = getComputedStyle(columnElement);
+
+      const gap = parseInt(containerStyles.getPropertyValue('gap'));
+      const columnWidth = parseInt(columnStyles.getPropertyValue('min-width'));
+      const containerWidth = parseInt(container.value.getBoundingClientRect().width);
+
+      // remove dummy element
+      container.value.removeChild(columnElement);
+
+      const count = columnWidth ? Math.floor((containerWidth + gap) / (columnWidth + gap)) : 1;
+
+      return count > 0 ? count : 1;
     }
-  },
-  beforeDestroy() {
-    const vm = this;
-    if (process.isClient) {
-      if (!window.CSS.supports('grid-template-rows', 'masonry')) {
-        vm.masonryEvents.forEach((event) => {
-          window.removeEventListener(event, vm.resizeAllMasonryItems);
-        });
+
+    function createColumns(count) {
+      return [...new Array(count)].map(() => []);
+    }
+
+    function fillColumns(itemIndex) {
+      if (itemIndex >= items.value.length) {
+        return;
       }
+      nextTick(() => {
+        const columnElements = [...container.value.children];
+        const target = columnElements.reduce((prev, curr) =>
+          curr.getBoundingClientRect().height < prev.getBoundingClientRect().height ? curr : prev
+        );
+        columns.value[target.dataset.index].push(itemIndex);
+        fillColumns(itemIndex + 1);
+      });
     }
+
+    function redraw(force = false) {
+      if (columns.value.length === columnCount() && !force) {
+        return;
+      }
+      columns.value = createColumns(columnCount());
+      fillColumns(0);
+    }
+
+    columns.value = [items.value.map((_item, index) => index)];
+
+    onMounted(() => {
+      const supportsGridMasonry = window.CSS.supports('grid-template-rows', 'masonry');
+      if (!supportsGridMasonry) {
+        isJsMasonry.value = true;
+        resizeObserver.value = new ResizeObserver(() => redraw());
+        columnCount();
+        redraw();
+        resizeObserver.value.observe(container.value);
+      }
+    });
+
+    onBeforeUnmount(() => {
+      if (resizeObserver.value) {
+        resizeObserver.value.unobserve(container.value);
+      }
+    });
+
+    return {
+      items,
+      columns,
+      container,
+      isJsMasonry,
+    };
   },
 };
 </script>
@@ -109,28 +126,57 @@ export default {
 @import '../../assets/styles/tools';
 
 .sp-c-card-masonry {
+  &.is-js-masonry {
+    display: flex;
+    width: 100%;
+    gap: var(--space-y-2) var(--space-x-2);
+
+    @media (--mq-sm) {
+      gap: var(--space-y-4) var(--space-x-4);
+    }
+  }
+}
+
+.sp-c-card-masonry__column {
   display: grid;
   grid-gap: var(--space-y-2) var(--space-x-2);
   grid-template-rows: masonry;
   grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
 
+  .is-js-masonry & {
+    display: flex;
+    flex-basis: 0;
+    flex-direction: column;
+    flex-grow: 1;
+    height: max-content;
+    min-width: 150px;
+    gap: var(--space-y-2) var(--space-x-2);
+  }
+
   @media (--mq-sm) {
     grid-gap: var(--space-y-4) var(--space-x-4);
     grid-template-columns: repeat(auto-fill, minmax(270px, 1fr));
+
+    .is-js-masonry & {
+      gap: var(--space-y-4) var(--space-x-4);
+      min-width: 270px;
+    }
   }
 
   @media (--mq-lg) {
     grid-template-columns: repeat(auto-fill, minmax(320px, 1fr));
+
+    .is-js-masonry & {
+      min-width: 320px;
+    }
   }
 
   @media (--mq-xxxl) {
     grid-template-columns: repeat(auto-fill, minmax(450px, 1fr));
-  }
-}
 
-.sp-c-card-masonry__item {
-  .is-calculated & {
-    background-color: #ffffff;
+    .is-js-masonry & {
+      min-width: 450px;
+    }
   }
 }
 </style>
